9687ad4db149a1ff2e388fdceec12a31cd10f4cf
David Stinnett <davidstinnett@icloud.com>
2022-04-13 13:26:30 -0400

    basic layout. Need to add a nice font, set up CSS animations and finalize text. Will also get some kind of button to link to list of excursions

---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import HeroText from "./Herotext";
+
+import "../index.css";
+
+const Hero = () => {
+  return (
+    <main className="hero">
+      <HeroText />
+    </main>
+  );
+};
+
+export default Hero;
