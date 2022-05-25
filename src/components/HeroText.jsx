258e2a169c0205e27a97c6e66c3c643e42cb56eb
David Stinnett <davidstinnett@icloud.com>
2022-05-24 21:16:26 -0400

    finished out styling, just need content

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -16,7 +16,7 @@ const HeroText = () => {
             <h4 className="splashy">Welcome!!!</h4>
             <div className="heroLine" />
             <section className="paragraph heroParagraph">
-              <p>
+              <p className="heroParagraph">
                 I am blessed to live in a tropical paradise and it would be my pleasure to share this with you! Whether you want a relaxing day on the beach, to enjoy a friendly atmosphere on an island bar, or an adventure in the jungle, I can help make your vacation magical!
               </p>
             </section>
