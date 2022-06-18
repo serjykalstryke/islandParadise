925823ac7cfef9d013b479424f694f50e2f6ad8b
David Stinnett <davidstinnett@icloud.com>
2022-06-17 22:27:54 -0400

    added images to site

--- src/components/Excursions.jsx
+++ src/components/Excursions.jsx
@@ -2,13 +2,21 @@ import { Row, Col, ListGroup, Tab, Image } from "react-bootstrap";
 
 const Excursions = () => {
   return (
-    <div className="excursionsContainer px-4 justify-content-evenly" id="excursions">
-      <Tab.Container id="list-group-tabs-example tabContainer" defaultActiveKey="#link1">
+    <div
+      className="excursionsContainer px-4 justify-content-evenly"
+      id="excursions"
+    >
+      <Tab.Container
+        id="list-group-tabs-example tabContainer"
+        defaultActiveKey="#link1"
+      >
         <Row className="tabs row row-cols-lg-5 g-2">
           <Col lg={3} className="p-4">
             <ListGroup>
               <ListGroup.Item action href="#link1">
-                <span className="excursionTitle">Horse Back Riding, Sloth Hangout and Tour</span>
+                <span className="excursionTitle">
+                  Horse Back Riding, Sloth Hangout and Tour
+                </span>
               </ListGroup.Item>
               <ListGroup.Item action href="#link2">
                 <span className="excursionTitle">Sloth and Monkey Hangout</span>
@@ -23,18 +31,16 @@ const Excursions = () => {
                 <span className="excursionTitle">ATV Tours</span>
               </ListGroup.Item>
               <ListGroup.Item action href="#link6">
-                <span className="excursionTitle">Little French Key Day Pass</span>
+                <span className="excursionTitle">
+                  Little French Key Day Pass
+                </span>
               </ListGroup.Item>
             </ListGroup>
           </Col>
           <Col lg={9} className="p-10">
             <Tab.Content>
               <Tab.Pane eventKey="#link1">
-                <Image
-                  className="excursionOneImage"
-                  src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d2d9b0d3-236d-488f-8b8f-5b05eedb717c/ddb8ugy-371e8371-d327-47bb-9ae5-49f822988301.png/v1/fill/w_1192,h_670,q_70,strp/beach_day_thumbnail_by_d_reamzrblx_ddb8ugy-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6IlwvZlwvZDJkOWIwZDMtMjM2ZC00ODhmLThiOGYtNWIwNWVlZGI3MTdjXC9kZGI4dWd5LTM3MWU4MzcxLWQzMjctNDdiYi05YWU1LTQ5ZjgyMjk4ODMwMS5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.NSeAsZ36bskI4buLsM-WpGqkPUDzKp4UbftmiB_ffiI"
-                  thumbnail
-                />{" "} 
+                <div className="excursionOneImage"></div>{" "}
                 <p className="excursionText">
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                   Nesciunt quia ipsam quaerat iusto tempore ipsum esse ad
@@ -45,11 +51,7 @@ const Excursions = () => {
                 </p>
               </Tab.Pane>
               <Tab.Pane eventKey="#link2">
-                <Image
-                  className="excursionOneImage"
-                  src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d2d9b0d3-236d-488f-8b8f-5b05eedb717c/ddb8ugy-371e8371-d327-47bb-9ae5-49f822988301.png/v1/fill/w_1192,h_670,q_70,strp/beach_day_thumbnail_by_d_reamzrblx_ddb8ugy-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6IlwvZlwvZDJkOWIwZDMtMjM2ZC00ODhmLThiOGYtNWIwNWVlZGI3MTdjXC9kZGI4dWd5LTM3MWU4MzcxLWQzMjctNDdiYi05YWU1LTQ5ZjgyMjk4ODMwMS5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.NSeAsZ36bskI4buLsM-WpGqkPUDzKp4UbftmiB_ffiI"
-                  thumbnail
-                />{" "}
+                <div className="excursionTwoImage"></div>{" "}
                 <p className="excursionText">
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                   Nesciunt quia ipsam quaerat iusto tempore ipsum esse ad
@@ -60,11 +62,7 @@ const Excursions = () => {
                 </p>
               </Tab.Pane>
               <Tab.Pane eventKey="#link3">
-                <Image
-                  className="excursionOneImage"
-                  src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d2d9b0d3-236d-488f-8b8f-5b05eedb717c/ddb8ugy-371e8371-d327-47bb-9ae5-49f822988301.png/v1/fill/w_1192,h_670,q_70,strp/beach_day_thumbnail_by_d_reamzrblx_ddb8ugy-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6IlwvZlwvZDJkOWIwZDMtMjM2ZC00ODhmLThiOGYtNWIwNWVlZGI3MTdjXC9kZGI4dWd5LTM3MWU4MzcxLWQzMjctNDdiYi05YWU1LTQ5ZjgyMjk4ODMwMS5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.NSeAsZ36bskI4buLsM-WpGqkPUDzKp4UbftmiB_ffiI"
-                  thumbnail
-                />{" "}
+                <div className="excursionThreeImage"></div>{" "}
                 <p className="excursionText">
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                   Nesciunt quia ipsam quaerat iusto tempore ipsum esse ad
@@ -75,11 +73,7 @@ const Excursions = () => {
                 </p>
               </Tab.Pane>
               <Tab.Pane eventKey="#link4">
-                <Image
-                  className="excursionOneImage"
-                  src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d2d9b0d3-236d-488f-8b8f-5b05eedb717c/ddb8ugy-371e8371-d327-47bb-9ae5-49f822988301.png/v1/fill/w_1192,h_670,q_70,strp/beach_day_thumbnail_by_d_reamzrblx_ddb8ugy-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6IlwvZlwvZDJkOWIwZDMtMjM2ZC00ODhmLThiOGYtNWIwNWVlZGI3MTdjXC9kZGI4dWd5LTM3MWU4MzcxLWQzMjctNDdiYi05YWU1LTQ5ZjgyMjk4ODMwMS5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.NSeAsZ36bskI4buLsM-WpGqkPUDzKp4UbftmiB_ffiI"
-                  thumbnail
-                />{" "}
+                <div className="excursionFourImage"></div>{" "}
                 <p className="excursionText">
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                   Nesciunt quia ipsam quaerat iusto tempore ipsum esse ad
@@ -90,11 +84,7 @@ const Excursions = () => {
                 </p>
               </Tab.Pane>
               <Tab.Pane eventKey="#link5">
-                <Image
-                  className="excursionOneImage"
-                  src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d2d9b0d3-236d-488f-8b8f-5b05eedb717c/ddb8ugy-371e8371-d327-47bb-9ae5-49f822988301.png/v1/fill/w_1192,h_670,q_70,strp/beach_day_thumbnail_by_d_reamzrblx_ddb8ugy-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6IlwvZlwvZDJkOWIwZDMtMjM2ZC00ODhmLThiOGYtNWIwNWVlZGI3MTdjXC9kZGI4dWd5LTM3MWU4MzcxLWQzMjctNDdiYi05YWU1LTQ5ZjgyMjk4ODMwMS5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.NSeAsZ36bskI4buLsM-WpGqkPUDzKp4UbftmiB_ffiI"
-                  thumbnail
-                />{" "}
+                <div className="excursionFiveImage"></div>{" "}
                 <p className="excursionText">
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                   Nesciunt quia ipsam quaerat iusto tempore ipsum esse ad
@@ -105,11 +95,7 @@ const Excursions = () => {
                 </p>
               </Tab.Pane>
               <Tab.Pane eventKey="#link6">
-                <Image
-                  className="excursionOneImage"
-                  src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d2d9b0d3-236d-488f-8b8f-5b05eedb717c/ddb8ugy-371e8371-d327-47bb-9ae5-49f822988301.png/v1/fill/w_1192,h_670,q_70,strp/beach_day_thumbnail_by_d_reamzrblx_ddb8ugy-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6IlwvZlwvZDJkOWIwZDMtMjM2ZC00ODhmLThiOGYtNWIwNWVlZGI3MTdjXC9kZGI4dWd5LTM3MWU4MzcxLWQzMjctNDdiYi05YWU1LTQ5ZjgyMjk4ODMwMS5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.NSeAsZ36bskI4buLsM-WpGqkPUDzKp4UbftmiB_ffiI"
-                  thumbnail
-                />{" "}
+                <div className="excursionSixImage"></div>{" "}
                 <p className="excursionText">
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                   Nesciunt quia ipsam quaerat iusto tempore ipsum esse ad
